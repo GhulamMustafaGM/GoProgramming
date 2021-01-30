4dd7c26adf235cba76c1999370b179ac73f57d74
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-30 01:32:26 +0100

    Int Program and StringProgram

---
+++ IntProgram.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	a = 18
+
+	fmt.Println("The value of a:", a)
+}
---
+++ StringProgram.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string
+	a = "String presentation"
+
+	fmt.Println("The value of a:", a)
+}
